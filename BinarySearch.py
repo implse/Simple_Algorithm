274447c9c1a1c17eeb6c9d1a98bf782a59c0e3a7
Finite <finite.impulse@gmail.com>
2018-04-27 05:42:27 +0200

    Add Binary Search

---
+++ BinarySearch.py
@@ -0,0 +1,20 @@
+# Binary Search
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        value = list[mid]
+        if value == item:
+            return "The item %d is at index: %d" % (item, mid)
+        if value > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return "The item %d is not in the list." % (item)
+
+# Test
+b1 = [1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71]
+print(binary_search(b1, 38))
