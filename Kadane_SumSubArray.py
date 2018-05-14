4070463dfb457619100982d9abae9204bc6b9987
Finite <finite.impulse@gmail.com>
2018-05-14 22:43:28 +0200

    Add Kadane findmaxSumSubArray & findMaxSum

--- Kadane_SumSubArray.py
+++ Kadane_SumSubArray.py
@@ -1,25 +1,7 @@
 # Kadane Algorithm (Largest Sum Contigous Sub Array)
 
 # Find the subarray with the maximum sum in an array.
-
-def Kadane(arr):
-    max_so_far = arr[0]
-    max_ending_here = 0
-    start = 0
-    end = 0
-    s = 0
-    for i in arr:
-        max_ending_here += i
-        if max_so_far < max_ending_here:
-            max_so_far = max_ending_here
-            start = s
-            end = arr.index(i)
-        if max_ending_here < 0:
-            max_ending_here = 0
-            s = arr.index(i) + 1
-    return arr[start:end+1]
-
-def Kadane(arr):
+def findmaxSumSubArray(arr):
     max_so_far = arr[0]
     max_ending_here = 0
     start = 0
@@ -36,8 +18,24 @@ def Kadane(arr):
             s = i + 1
     return arr[start:end+1]
 
-# Test
+# Find the maximum sum in an array
+def findMaxSum(arr):
+        max_so_far = arr[0]
+        max_ending_here = 0
+        for i in range(len(arr)):
+            max_ending_here += arr[i]
+            if max_so_far < max_ending_here:
+                max_so_far = max_ending_here
+            if max_ending_here < 0:
+                max_ending_here = 0
+        return max_so_far
 
+# Test 1
 a = [4, -3, -2, 2, 3, 1, -2, -3, 4, 2, -6, -3, -1, 3, 1, 2]
+print('The maximum subarray sum is : {}'.format(findMaxSum(a)))
+print('The maximul sum subarray is : {}'.format(findmaxSumSubArray(a)))
 
-print(Kadane(a))
+# Test 2
+b = [-5, 6, 7, 1, 4, -8, 16]
+print('The maximum subarray sum is : {}'.format(findMaxSum(b)))
+print('The maximul sum subarray is : {}'.format(findmaxSumSubArray(b)))
