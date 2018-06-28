33d8c7aaa5d9d26b4a079fb4c3ceabae61acd3d4
Finite <finite.impulse@gmail.com>
2018-06-28 21:11:30 +0200

    Add Heapsort

---
+++ Heapsort.py
@@ -0,0 +1,50 @@
+# Heapsort - Max Heap
+
+# Heapify
+def heapify(arr, sz, i):
+
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+
+    # See if left child of root exists and is
+    # greater than root
+    if l < sz and arr[i] < arr[l]:
+        largest = l
+
+    # See if right child of root exists and is
+    # greater than root
+    if r < sz and arr[largest] < arr[r]:
+        largest = r
+
+    # Change root, if needed
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap
+
+        # Heapify the root.
+        heapify(arr, sz, largest)
+
+# Heapsort
+def heapSort(arr):
+    sz = len(arr)
+
+    # Build a Max Heap.
+    for i in range(sz, -1, -1):
+        heapify(arr, sz, i)
+
+    # One by one extract elements
+    for i in range(sz - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        heapify(arr, i, 0)
+
+# Test
+arr = [4, 8, 11, 25, 2, 17, 74, 51]
+
+print("Initial array :")
+print(arr)
+
+heapSort(arr)
+sz = len(arr)
+
+print ("Sorted array :")
+print(arr)
