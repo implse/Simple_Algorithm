d72a1fcdf63d752421bd9f43f524f7cfdbc2d72d
Finite <finite.impulse@gmail.com>
2019-04-05 21:22:47 +0200

    Add Permutations

---
+++ Permutations.py
@@ -0,0 +1,20 @@
+def permutations(lst):
+    result = list()
+    permutionsHelper(lst, result, chosen = [])
+    return result
+
+def permutionsHelper(lst, result, chosen = []):
+    if len(lst) == 0:
+        result.append(chosen.copy())
+    else:
+        for i in range(len(lst)):
+            c = lst[i]
+            chosen.append(c)
+            lst.pop(i)
+            permutionsHelper(lst, result, chosen)
+            chosen.pop()
+            lst.insert(i, c)
+
+# Test
+l = [1, 2, 3]
+print(permutations(l))
